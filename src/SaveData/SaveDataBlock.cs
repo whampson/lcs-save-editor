f576e55c538397076bbec0250793a9ac90d618e1
Wes Hampson <thehambone93@gmail.com>
2018-06-30 21:47:50 -0700

    Rename file

--- src/SaveData/SaveDataBlock.cs
+++ src/SaveData/SaveDataBlock.cs
@@ -1,16 +1,16 @@
 ﻿#region License
 /* Copyright(c) 2016-2018 Wes Hampson
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,11 +25,11 @@ using Newtonsoft.Json;
 
 namespace WHampson.LcsSaveEditor.SaveData
 {
-    internal abstract class DataBlock
+    internal abstract class SaveDataBlock
     {
         protected readonly BlockHeader header;
 
-        public DataBlock()
+        public SaveDataBlock()
         {
             header = new BlockHeader();
         }
