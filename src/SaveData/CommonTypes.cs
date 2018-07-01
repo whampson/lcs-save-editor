e63f36ab98bf4095702aac6605a0af3af2a7539b
Wes Hampson <thehambone93@gmail.com>
2018-06-30 21:49:07 -0700

    BlockHeader: rename Size to BlockSize

--- src/SaveData/CommonTypes.cs
+++ src/SaveData/CommonTypes.cs
@@ -1,16 +1,16 @@
 ﻿#region License
 /* Copyright(c) 2016-2018 Wes Hampson
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,14 +28,14 @@ namespace WHampson.LcsSaveEditor.SaveData
     internal class BlockHeader
     {
         private readonly Primitive<Char8> tag;
-        private readonly Primitive<uint> size;
+        private readonly Primitive<uint> blockSize;
 
         private StringWrapper tagWrapper;
 
         public BlockHeader()
         {
             tag = new Primitive<Char8>(null, 0);
-            size = new Primitive<uint>(null, 0);
+            blockSize = new Primitive<uint>(null, 0);
         }
 
         public string Tag
@@ -54,10 +54,10 @@ namespace WHampson.LcsSaveEditor.SaveData
             }
         }
 
-        public uint Size
+        public uint BlockSize
         {
-            get { return size.Value; }
-            set { size.Value = value; }
+            get { return blockSize.Value; }
+            set { blockSize.Value = value; }
         }
     }
 
