e684ec92abe49ea130584e45e5f8c054e983398e
Wes Hampson <thehambone93@gmail.com>
2018-06-30 21:56:58 -0700

    Unimportant testing stuff

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,16 @@
 ﻿#region License
 /* Copyright(c) 2016-2018 Wes Hampson
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -60,7 +60,9 @@ namespace WHampson.LcsSaveEditor
             flags |= DeserializationFlags.NonPublic;
             SaveGame sg = data.Deserialize<SaveGame>(flags);
             MessageBox.Show(writer.ToString().Replace('\0', ' '));
-            File.WriteAllText("out.txt", sg.ToString());
+            File.WriteAllText("out.json", sg.ToString());
+
+            // sg.Scripts.RunningScripts[43].ReturnStack[5] = 4;
 
 
         }
@@ -72,14 +74,20 @@ namespace WHampson.LcsSaveEditor
     class SaveGame
     {
         private SimpleVarsBlock simpleVars;
-        private ScriptBlock scripts;
+        private ScriptsBlock scripts;
+
+        public SaveGame()
+        {
+            simpleVars = new SimpleVarsBlock();
+            scripts = new ScriptsBlock();
+        }
 
         public SimpleVarsBlock SimpleVars
         {
             get { return simpleVars; }
         }
 
-        public ScriptBlock Scripts
+        public ScriptsBlock Scripts
         {
             get { return scripts; }
         }
