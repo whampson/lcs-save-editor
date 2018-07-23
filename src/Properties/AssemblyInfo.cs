2a52490b7c393dda61312de9d7a0a5f66049521f
Wes Hampson <thehambone93@gmail.com>
2018-07-22 21:56:27 -0700

    Bump version to 0.2.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly   // generic resource dictionary 
 )]
 
-[assembly: AssemblyVersion("0.1.0.267")]
-[assembly: AssemblyFileVersion("0.1.0.267")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0.268")]
+[assembly: AssemblyFileVersion("0.2.0.268")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
