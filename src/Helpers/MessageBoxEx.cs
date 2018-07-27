d385e47afa102dc64777f49fec2eebb78b420954
Wes Hampson <thehambone93@gmail.com>
2018-07-27 13:21:15 -0700

    Add license header and disable warning

--- src/Helpers/MessageBoxEx.cs
+++ src/Helpers/MessageBoxEx.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿#region License
+/* Copyright(c) 2016-2018 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -111,9 +134,11 @@ namespace WHampson.LcsSaveEditor.Helpers
                 throw new NotSupportedException("Multiple calls are not supported.");
             }
 
+#pragma warning disable 0618    // GetCurrentThreadId() still works for what we need it to do.
             if (_owner != null) {
                 _hHook = SetWindowsHookEx(WH_CALLWNDPROCRET, _hookProc, IntPtr.Zero, AppDomain.GetCurrentThreadId());
             }
+#pragma warning restore 0618
         }
 
         private static IntPtr MessageBoxHookProc(int nCode, IntPtr wParam, IntPtr lParam)
