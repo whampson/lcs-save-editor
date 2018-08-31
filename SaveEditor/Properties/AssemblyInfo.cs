74ef8c36faf65227852f793f51ea034c49156583
Wes Hampson <thehambone93@gmail.com>
2018-08-31 14:06:44 -0500

    Rename folder

--- SaveEditor/Properties/AssemblyInfo.cs
+++ SaveEditor/Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly   // generic resource dictionary 
 )]
 
-[assembly: AssemblyVersion("0.3.0.352")]
-[assembly: AssemblyFileVersion("0.3.0.353")]
+[assembly: AssemblyVersion("0.3.0.356")]
+[assembly: AssemblyFileVersion("0.3.0.356")]
 [assembly: AssemblyInformationalVersion("0.3.0")]
