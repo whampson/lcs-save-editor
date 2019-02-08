8e46dafb5e76abe30c2030268e68af16f99e4f26
Wes Hampson <thehambone93@gmail.com>
2019-02-08 00:59:09 -0600

    Add MainWindow

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region License
+/* Copyright(c) 2016-2019 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using LcsSaveEditor.DataTypes;
+using LcsSaveEditor.ViewModels;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LcsSaveEditor.Views
 {
@@ -23,6 +36,38 @@ namespace LcsSaveEditor.Views
         public MainWindow()
         {
             InitializeComponent();
+
+            ViewModel.MessageBoxRequested += ViewModel_MessageBoxRequested;
+            ViewModel.FileDialogRequested += ViewModel_FileDialogRequested;
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel) DataContext; }
+            set { DataContext = value; }
+        }
+
+        private void ViewModel_MessageBoxRequested(object sender, MessageBoxEventArgs e)
+        {
+            e.Show(this);
+        }
+
+        private void ViewModel_FileDialogRequested(object sender, FileDialogEventArgs e)
+        {
+            e.ShowDialog(this);
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            // Close current file
+            if (ViewModel.IsFileOpen) {
+                ViewModel.CloseFileCommand.Execute(null);
+            }
+
+            // Only close window if user didn't cancel closing file
+            if (ViewModel.IsFileOpen) {
+                e.Cancel = true;
+            }
         }
     }
 }
