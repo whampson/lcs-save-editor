953b9d6bf536d477be526eb77ed65d501441536e
Wes Hampson <thehambone93@gmail.com>
2019-02-10 22:13:46 -0600

    Add DataGrid context menu

--- Views/GlobalVariablesView.xaml.cs
+++ Views/GlobalVariablesView.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region License
+/* Copyright(c) 2016-2019 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using LcsSaveEditor.ViewModels;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LcsSaveEditor.Views
 {
@@ -26,9 +40,41 @@ namespace LcsSaveEditor.Views
             InitializeComponent();
         }
 
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Register items source CollectionChanged listener
+
+            if (!(dg.ItemsSource is ObservableCollection<NamedScriptVariable> itemsSource)) {
+                return;
+            }
+            itemsSource.CollectionChanged += ItemsSource_CollectionChanged;
+        }
+
         private void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
         {
-            e.Row.Header = e.Row.GetIndex().ToString();
+            // Put row number in row header
+
+            int idx = e.Row.GetIndex();
+            if (idx == dg.Items.Count - 1) {
+                e.Row.Header = "*";
+            }
+            else {
+                e.Row.Header = idx.ToString();
+            }
+        }
+
+        private void ItemsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            // Refresh the rows each time an item is added/removed.
+            // This needs to happen otherwise the row numbers will have a gap or duplicate
+            // until the user has scrolled far enough away to trigger another refresh.
+
+            switch (e.Action) {
+                case NotifyCollectionChangedAction.Add:
+                case NotifyCollectionChangedAction.Remove:
+                    dg.Items.Refresh();
+                    break;
+            }
         }
     }
 }
