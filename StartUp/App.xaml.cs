3d08d74598ae5acb46a9ef8c9d8676edbe30002b
Wes Hampson <thehambone93@gmail.com>
2019-02-14 11:19:43 -0600

    Add some log messages; add license header

--- StartUp/App.xaml.cs
+++ StartUp/App.xaml.cs
@@ -1,10 +1,31 @@
-﻿using LcsSaveEditor.Helpers;
+﻿#region License
+/* Copyright(c) 2016-2019 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
 using LcsSaveEditor.Infrastructure;
 using LcsSaveEditor.Resources;
 using LcsSaveEditor.Views;
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Threading;
@@ -30,11 +51,19 @@ namespace LcsSaveEditor.StartUp
             Logger.ConsumeStandardOut();
             Logger.ConsumeStandardError();
 
-            Logger.Info(Strings.AppName);
-            Logger.Info("Version {0}", GetAppVersionString());
+            Logger.Info("========== {0} ==========", Strings.AppName);
+            Logger.Info("Version: {0}", GetAppVersionString());
+            Logger.Info("Host OS: {0}", Environment.OSVersion);
+            Logger.Info("==========={0}===========", new string('=', Strings.AppName.Length));
+            Logger.Info("Loading UI...");
 
             MainWindow = new MainWindow();
             MainWindow.Show();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Logger.Info("Exiting...");
         }
 
         private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
