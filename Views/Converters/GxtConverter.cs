cf95094acd7026a50a0ee85507cb339f7a983d21
Wes Hampson <thehambone93@gmail.com>
2019-03-31 12:50:08 -0700

    Add GxtSelectionWindow and GxtConverter

---
+++ Views/Converters/GxtConverter.cs
@@ -0,0 +1,52 @@
+﻿#region License
+/* Copyright(c) 2016-2019 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using LcsSaveEditor.Core.Helpers;
+using LcsSaveEditor.Resources;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LcsSaveEditor.Views.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class GxtConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string key = value as string;
+            bool hasKey = GxtHelper.CurrentMainTable.TryGetValue(key, out string gxtValue);
+
+            if (hasKey) {
+                return gxtValue;
+            }
+
+            return "(invalid GXT key)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException(CommonResources.Error_ConvertBackNotSupported);
+        }
+    }
+}
---
+++ Views/GxtSelectionWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿#region License
+/* Copyright(c) 2016-2019 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using LcsSaveEditor.ViewModels;
+using System.Windows;
+
+namespace LcsSaveEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for GxtSelectionWindow.xaml
+    /// </summary>
+    public partial class GxtSelectionWindow : Window
+    {
+        public GxtSelectionWindow()
+        {
+            InitializeComponent();
+
+            ViewModel.DialogCloseRequested += ViewModel_DialogCloseRequested;
+        }
+
+        public GxtViewModel ViewModel
+        {
+            get { return (GxtViewModel) DataContext; }
+            set { DataContext = value; }
+        }
+
+        private void ViewModel_DialogCloseRequested(object sender, DialogCloseEventArgs e)
+        {
+            DialogResult = e.DialogResult;
+            Close();
+        }
+    }
+}
