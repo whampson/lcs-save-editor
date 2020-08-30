c19aea8c55d2825a882a183a493a3ab21bcd358e
Wes Hampson <thehambone93@gmail.com>
2020-08-30 15:07:21 -0700

    Add carcols support

---
+++ LCSSaveEditor.Core/Carcols.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace LCSSaveEditor.Core
+{
+    public class Carcols
+    {
+        public static Carcols TheCarcols { get; private set; }
+
+        static Carcols()
+        {
+            TheCarcols = new Carcols();
+        }
+
+        private List<Color> m_colors;
+
+        public Carcols()
+        {
+            m_colors = new List<Color>();
+        }
+
+        public List<Color> Colors
+        {
+            get { return m_colors; }
+        }
+
+        public void Load(string path)
+        {
+            Load(File.ReadAllBytes(path));
+        }
+
+        public void Load(byte[] data)
+        {
+            // TOOD: error handling?
+
+            using (MemoryStream m = new MemoryStream(data))
+            {
+                using (StreamReader buf = new StreamReader(m))
+                {
+                    string line;
+                    while (buf.ReadLine().Trim() != "col") { }
+
+                    while ((line = buf.ReadLine().Trim()) != "end")
+                    {
+                        string[] rgb = line.Split(' ', '\t');
+                        int.TryParse(rgb[0], out int r);
+                        int.TryParse(rgb[1], out int g);
+                        int.TryParse(rgb[2], out int b);
+
+                        m_colors.Add(Color.FromArgb(r, g, b));
+                    }
+                }
+            }
+        }
+    }
+}
